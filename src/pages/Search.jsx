75037634f0630cc8514d7afd0226c67a8a253813
MalluCortez <maria.santana@outlook.com.br>
2022-11-15 01:46:24 -0300

    requisito 5 feito

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,56 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  state = {
+    searchArtist: '',
+    isDisabled: true,
+  };
+
+  VerificationSearchArtistButton = () => {
+    const { searchArtist } = this.state;
+    const minimumNumber = 2;
+    const verificationArtistName = searchArtist.length < minimumNumber;
+    this.setState({
+      isDisabled: verificationArtistName,
+    });
+  };
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    }, () => {
+      this.VerificationSearchArtistButton();
+    });
+  };
+
   render() {
+    const { searchArtist, isDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <label htmlFor="search-artist-input">
+            Nome da Banda ou Artista
+            <input
+              type="text"
+              id="search-artist-input"
+              data-testid="search-artist-input"
+              name="searchArtist"
+              value={ searchArtist }
+              onChange={ this.handleChange }
+            />
+          </label>
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            id="search-artist-button"
+            disabled={ isDisabled }
+            /* onClick={ } */
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
